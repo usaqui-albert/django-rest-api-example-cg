bcdc3a5e989e662ba2d99276c8e4ea867aa2e768
Albert Usaqui <albert.usaqui@blanclink.com>
2016-07-01 16:05:50 -0400

    changing some user model fields, adding some tests of the user model methods

--- countries/models.py
+++ countries/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Country(models.Model):
+    """Model for Country Object"""
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
 
--- users/models.py
+++ users/models.py
@@ -1,10 +1,14 @@
+import datetime
+
 from django.contrib.auth.models import AbstractBaseUser, UserManager
 from django.db import models
+from django.utils import timezone
 
 from countries.models import Country
 from cities.models import City
 
 class User(AbstractBaseUser):
+    """Model for User object"""
     email = models.EmailField(max_length=100, unique=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -14,10 +18,11 @@ class User(AbstractBaseUser):
     city = models.OneToOneField(City, related_name='city')
     facebook = models.CharField(max_length=100, null=True)
     twitter = models.CharField(max_length=100, null=True)
-    linked_in = models.CharField(max_length=100, null=True)
+    linkedin = models.CharField(max_length=100, null=True)
     phone_number = models.CharField(max_length=100)
     is_active = models.BooleanField(default=False)
-    is_premium = models.BooleanField(default=False)
+    free_trial_started_at = models.DateTimeField(null=True)
+    free_trial_used = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -28,3 +33,6 @@ class User(AbstractBaseUser):
 
     def __str__(self):
         return self.email
+
+    def free_trial_is_over(self):
+        return self.free_trial_started_at <= timezone.now() - datetime.timedelta(days=15)
