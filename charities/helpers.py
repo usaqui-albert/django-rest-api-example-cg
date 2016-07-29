423fe44fce476ad3c836a85ff0b7dea60bb18410
Albert Usaqui <albert.usaqui@blanclink.com>
2016-07-29 18:28:03 -0400

    fix in the PathAndRename class

--- charities/helpers.py
+++ charities/helpers.py
@@ -7,7 +7,7 @@ class PathAndRename(object):
     def __init__(self, sub_path):
         self.path = sub_path
 
-    def __call__(self, filename):
+    def __call__(self, instance, filename):
         ext = filename.split('.')[-1]
         filename = '{}.{}'.format(uuid4().hex, ext)
         return os.path.join(self.path, filename)
