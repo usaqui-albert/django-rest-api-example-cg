67093c2fd610d5baecbcec7bbc92ee346ea65c91
Albert Usaqui <albert.usaqui@blanclink.com>
2016-08-03 15:39:28 -0400

    testing xml response from benevity

--- testing_xml_requests.py
+++ testing_xml_requests.py
@@ -1,10 +1,10 @@
 import urllib2
-from xml.etree import ElementTree
+from lxml import etree, objectify
 
 url = 'https://sandbox.benevity.org/Adapter.General/1/ActivateUser?hmac=empty-key&user=User01120160331210259006&lastname=Tremaine&initials=J&firstname=Kimberly&email=KimberlyJTremaine@example.example&country=124&address-street=504SilverSpringsBlvd&address-state=AB&address-postcode=T3B2C3&address-country=124&active=yes'
-request = urllib2.Request(url)
+
 try:
-    u = urllib2.urlopen(request)
+    u = urllib2.urlopen(url).read()
 except (urllib2.URLError, urllib2.HTTPError) as e:
     if isinstance(e, urllib2.HTTPError):
         print 'es un http error'
@@ -12,9 +12,11 @@ except (urllib2.URLError, urllib2.HTTPError) as e:
         print 'es un url error'
     print e.reason
 else:
-    tree = ElementTree.parse(u)
-    print tree
-    rootElem = tree.getroot()
-    print rootElem
-    response = rootElem.find('response')
-    print response
+    print u
+    root = etree.XML(u)
+    print root
+    xml_string = etree.tostring(root)
+    print xml_string
+    response = objectify.fromstring(xml_string)
+    for i in response.request.param:
+        print i.get('name'), i.get('value')
