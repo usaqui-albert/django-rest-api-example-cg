4927480917875533ddff468b04734d210aa53d58
Albert Usaqui <albert.usaqui@blanclink.com>
2016-08-05 15:57:57 -0400

    library for benevity requests implemented and test file to hit benevity api from the server

--- events/helpers.py
+++ events/helpers.py
@@ -1,8 +1,6 @@
 import uuid
 from django.db import IntegrityError
 
-from ConnectGood.settings import BENEVITY_BASE_URL
-
 STATUS_OF_THE_EVENT = (('ACCEPTED', 'A'), ('REJECTED', 'R'))
 
 def validate_uuid4(uuid_string):
@@ -24,12 +22,3 @@ def update_event_status(user_event, event_status):
         return False
     else:
         return True
-
-def benevity_request_handler(company_id, operation, **query_params):
-    https_request = BENEVITY_BASE_URL + str(company_id) + '/' + operation
-    if len(query_params) > 0:
-        https_request += '?'
-        for key, value in query_params.iteritems():
-            https_request += key + '=' + value + '&'
-        https_request = https_request[:-1]
-    return https_request
