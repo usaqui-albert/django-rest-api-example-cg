bcf3b296b64967805b2a7fbe7cbf0753e3bb4ac5
Albert Usaqui <albert.usaqui@blanclink.com>
2016-09-15 17:38:31 -0400

    validating if is user account or corporate account also when the account is created

--- users/tasks.py
+++ users/tasks.py
@@ -7,7 +7,12 @@ from mandrill_script import send_mandrill_email
 @task(ignore_result=True)
 def post_create_user(user_id):
     user = User.objects.get(pk=user_id)
-    template_vars = [{'content': user.first_name, 'name': 'user'}]
+    template_vars = [
+        {
+            'content': str(user.company) if user.is_corporate_account() else str(user.first_name),
+            'name': 'user'
+        }
+    ]
     receiver = {'email': user.email,
                 'name': user.get_full_name(),
                 'type': 'to'}
