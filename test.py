40034a4c52815880025fdbce46157be9b04e03c2
Albert Usaqui <albert.usaqui@blanclink.com>
2016-09-29 14:26:31 -0400

    ...

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from ConnectGood.settings import BENEVITY_COMPANY_ID, BENEVITY_API_KEY
 from benevity_library import benevity
+from events.views import get_user_params
 from users.models import User
 
 from pprint import PrettyPrinter
@@ -9,23 +10,10 @@ pp = PrettyPrinter(indent=1)
 benevity.api_key = BENEVITY_API_KEY
 benevity.company_id = BENEVITY_COMPANY_ID
 
-def get_data(user):
-    return {
-        'email': str(user.email),
-        'firstname': str(user.company) if user.is_corporate_account() else str(user.first_name),
-        'lastname': '-' if user.is_corporate_account() else str(user.last_name),
-        'active': 'yes',
-        'user': str(user.benevity_id),
-        'country': str(user.get_country_iso_code()),
-        'address-city': str(user.city),
-        'address-country': str(user.get_country_iso_code()),
-        'address-postcode': str(user.zip_code),
-        'address-state': str(user.get_state_as_string()),
-        'address-street': str(user.street_address)
-    }
-
 bryan = User.objects.get(email='bryan@connectgood.net')
-data = get_data(bryan)
+data = get_user_params(bryan)
+data.pop('active')
+data.pop('country')
 
 user_detail = benevity.get_user_profile(user=str(bryan.benevity_id))
 pp.pprint(user_detail)
