51fc247e92ac45320491f2ae5d9d7b28073851fb
Albert Usaqui <albert.usaqui@blanclink.com>
2016-09-30 15:57:16 -0400

    adding a new method to the benevity library called get_cause_profile and testing it

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
 from ConnectGood.settings import BENEVITY_COMPANY_ID, BENEVITY_API_KEY
 from benevity_library import benevity
-from events.views import get_user_params
-from users.models import User
 
 from pprint import PrettyPrinter
 
@@ -10,16 +8,5 @@ pp = PrettyPrinter(indent=1)
 benevity.api_key = BENEVITY_API_KEY
 benevity.company_id = BENEVITY_COMPANY_ID
 
-bryan = User.objects.get(email='bryan@connectgood.net')
-data = get_user_params(bryan)
-data.pop('active')
-data.pop('country')
-
-user_detail = benevity.get_user_profile(user=str(bryan.benevity_id))
-pp.pprint(user_detail)
-
-user_updated = benevity.update_user(**data)
-pp.pprint(user_updated)
-
-user_detail = benevity.get_user_profile(user=str(bryan.benevity_id))
-pp.pprint(user_detail)
+cause = benevity.get_cause_profile(cause='124-119219814RR0001')
+pp.pprint(cause)
